90037880c01a699ceae84972ff7d289303c7e898
Simo <s0551187@htw-berlin.de>
2022-07-19 18:47:13 +0200

    Sidebar components and Dark Mode with styled-components

--- youtube_clone/src/App.js
+++ youtube_clone/src/App.js
@@ -1,22 +1,36 @@
-import styled from "styled-components";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./utils/Theme";
 
 
 function App() {
-  return (
-    <Container>
-      {/** menu */}
-      <Menu />
 
-      {/** main */}
-      <Main>
-          <Navbar />
-          <Wrapper>
-            Videos cards
-          </Wrapper>
-      </Main>
-    </Container>
+  const [darkMode, setDarkMode] = useState(true);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme} >
+      <Container>
+        {/** menu */}
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+
+        {/** main */}
+        <Main>
+            <Navbar />
+            <Wrapper>
+              <h1>test</h1>
+              <h1>test</h1> 
+              <h1>test</h1> 
+              <h1>test</h1> 
+              <h1>test</h1> 
+              <h1>test</h1> 
+              <h1>test</h1> 
+              <h1>test</h1>          
+            </Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
@@ -29,6 +43,8 @@ const Container = styled.div`
 
 const Main = styled.div`
   flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({theme}) => theme.text}
 `;
 
 const Wrapper = styled.div``;
--- youtube_clone/src/components/Menu.jsx
+++ youtube_clone/src/components/Menu.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from '../img/logo.png'
+import HomeIcon from '@mui/icons-material/Home'
+import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined'
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptOutlined'
+import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined'
+import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined'
+import LibraryMusicOutlinedIcon from '@mui/icons-material/LibraryMusicOutlined'
+import SportsBasketballOutlinedIcon from '@mui/icons-material/SportsBasketballOutlined'
+import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined'
+import MovieOutlinedIcon from '@mui/icons-material/MovieOutlined'
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined'
+import LiveTvOutlinedIcon from '@mui/icons-material/LiveTvOutlined'
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
+import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined'
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined'
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
 
-function Menu() {
+
+
+function Menu({darkMode, setDarkMode}) {
   return (
     <Container>
         <Wrapper>
@@ -12,7 +30,77 @@ function Menu() {
             </Logo>
 
             <Item>
-                Menu
+                <HomeIcon />
+                Home
+            </Item>
+            <Item>
+                <ExploreOutlinedIcon />
+                Explore
+            </Item>
+            <Item>
+                <SubscriptionsOutlinedIcon />
+                Subscriptions
+            </Item>
+            <Hr />
+            <Item>
+                <VideoLibraryOutlinedIcon />
+                Library
+            </Item>
+            <Item>
+                <HistoryOutlinedIcon />
+                History
+            </Item>
+            <Hr />
+            <Login>
+                Sign in to like videos, comment, and subscribe
+                <Button>
+                    <AccountCircleOutlinedIcon />
+                    SIGN IN
+                </Button>
+            </Login>
+            <Hr />
+
+            <Title>Best of Youclone2.0</Title>
+            <Item>
+                <LibraryMusicOutlinedIcon />
+                Music
+            </Item>
+            <Item>
+                <SportsBasketballOutlinedIcon />
+                Sports
+            </Item>
+            <Item>
+                <SportsEsportsOutlinedIcon />
+                Gaming
+            </Item>
+            <Item>
+                <MovieOutlinedIcon />
+                Movies
+            </Item>
+            <Item>
+                <ArticleOutlinedIcon />
+                News
+            </Item>
+            <Item>
+                <LiveTvOutlinedIcon />
+                Live
+            </Item>
+            <Hr />
+            <Item>
+                <SettingsOutlinedIcon />
+                Settings
+            </Item>
+            <Item>
+                <FlagOutlinedIcon />
+                Report
+            </Item>
+            <Item>
+                <HelpOutlineOutlinedIcon />
+                Helps
+            </Item>
+            <Item onClick={()=>setDarkMode(!darkMode)} >
+                <SettingsBrightnessOutlinedIcon />
+                Light Mode
             </Item>
         </Wrapper>
     </Container>
@@ -23,10 +111,12 @@ export default Menu
 
 const Container = styled.div`
     flex: 1;
-    background-color: #202020;
-    color: #fff;
+    background-color: ${({theme}) =>theme.bg};
+    color: ${({theme}) =>theme.text};
     height: 100vh ;
     font-size: 14px;
+    position: sticky;
+    top: 0;
 `;
 
 const Wrapper = styled.div`
@@ -51,4 +141,34 @@ const Item = styled.div`
     align-items: center;
     gap: 20px;
     cursor: pointer;
+    padding: 7.5px 0px;
+`;
+
+const Hr = styled.hr`
+    margin: 15px 0px;
+    border: 0.5px solid ${({theme}) =>theme.soft};
+`;
+const Login = styled.div`
+
+`
+
+const Button = styled.button`
+    padding: 5px 15px;
+    background-color: transparent;
+    border: 1px solid #3ea6ff;
+    color: #3ea6ff;
+    border-radius: 3px;
+    font-weight: 500;
+    margin-top: 10px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`;
+
+const Title = styled.h2`
+    font-size: 14px;
+    font-weight: 500;
+    color: #aaaaaa;
+    margin-bottom: 20px;
 `;
